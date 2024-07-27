1bc1cf731f5cb0ccc9b075a7c06bcf55da3be518
Francesco <fotifgerend@gmail.com>
2024-07-27 18:56:55 +0200

    .

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -30,7 +30,6 @@ export default function Example() {
           <div className=" mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-15 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                
                 {/* Bottone menu mobile */}
                 <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -71,24 +70,23 @@ export default function Example() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <Link to={'/contact'}>
+                <Link to={"/contact"}>
                   <button
                     type="button"
                     className="relative text-sm rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                   >
-                    Contattamiu
+                    Contattami
                   </button>
                 </Link>
               </div>
             </div>
           </div>
-
+          {/* mobile navbar */}
           <DisclosurePanel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
                 <DisclosureButton
                   key={item.name}
-                  as="a"
                   href={item.href}
                   className={classNames(
                     item.current
